3dbf2ef84514d89e50438c27645b5b67d6a66fde
Morgan Nicol <morganichoui@gmail.com>
2023-06-12 18:39:35 +0200

    app -> front

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './global.scss';
 
 function App() {
   return (
@@ -8,10 +8,10 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>THIS IS MAINECOORSE</code>
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
+          :D
         </a>
       </header>
     </div>
