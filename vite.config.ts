17e5654ec6e31e636062cb948ab369cbfa381348
Morgan Nicol <morganichoui@gmail.com>
2023-06-12 16:41:03 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  css: { modules: { localsConvention: 'camelCase' } },
+  base: './',
+  server: {
+    host: 'localhost',
+    port: 1418,
+    strictPort: true,
+  },
+  preview: {
+    host: 'localhost',
+    port: 6744,
+  },
+  build: {
+    assetsInlineLimit: 50000,
+  },
+  // customLogger: logger,
+});
