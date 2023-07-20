172d8c969e4d487dd1622ee573a65028f375ffdc
Morgan Nicol <morganichoui@gmail.com>
2023-07-20 09:07:41 +0200

    OK for notes

--- apps/front/src/app/notes/notes.tsx
+++ apps/front/src/app/notes/notes.tsx
@@ -1,52 +1,22 @@
 import './notes.scss';
 import { TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 import { Loader } from '@components/loader/loader';
 import { DataError } from '@components/data-error/data-error';
-import { axiosUrl, configAxios, fetchApi, headers } from '@shared/hooks/axios.config';
-import useAxios from 'axios-hooks';
+import { configAxios } from '@shared/hooks/axios.config';
 
 export const Notes = (): JSX.Element => {
-  // const [{ data: getData, loading: getLoading, error: getError }, refetchData] =
-  //   configAxios({
-  //   url: 'notes',
-  //   method: 'GET',
-  //   autoCancel: false,
-  //   });
-
-  fetchApi('notes', 'GET')
-
-    //
-    // useAxios(
-    //   {
-    //     headers,
-    //     url: axiosUrl('notes'),
-    //     method: 'GET',
-    //   },
-    //   { autoCancel: false },
-    // );
+  const [{ data: getData, loading: getLoading, error: getError }] = configAxios({
+    url: 'notes',
+    method: 'GET',
+    autoCancel: false,
+  });
 
   // eslint-disable-next-line no-empty-pattern
-  const [{}, executePost] = useAxios(
-    {
-      url: axiosUrl('notes'),
-      method: 'POST',
-      headers,
-    },
-    { manual: true },
-  );
-
-  //
-  // eslint-disable-next-line no-empty-pattern
-  // const [{}, executePost] = configAxios({ url: 'notes', method: 'POST', manual: true });
+  const [{}, executePost] = configAxios({ url: 'notes', method: 'POST', manual: true });
 
   const [value, setValue] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    // refetchData();
-    console.log('eff');
-  });
 
   const handleChange = useDebouncedCallback(notes => {
     setValue(notes);
@@ -58,19 +28,19 @@ export const Notes = (): JSX.Element => {
   return (
     <div className='Notes'>
       <h2>Pense-bête</h2>
-      {/*{getLoading && <Loader />}*/}
-      {/*{getError && <DataError />}*/}
-      {/*{!getLoading && !getError && (*/}
-      {/*  <TextField*/}
-      {/*    variant='outlined'*/}
-      {/*    label='Notes'*/}
-      {/*    defaultValue={value ?? getData}*/}
-      {/*    placeholder='Pense, écrit, tape, romance, transcrit, compose, exprime, rédige, consigne, note... 🗒️'*/}
-      {/*    onChange={e => handleChange(e.target.value)}*/}
-      {/*    minRows={5}*/}
-      {/*    multiline*/}
-      {/*  ></TextField>*/}
-      {/*)}*/}
+      {getLoading && <Loader />}
+      {getError && <DataError />}
+      {!getLoading && !getError && (
+        <TextField
+          variant='outlined'
+          label='Notes'
+          defaultValue={value ?? getData}
+          placeholder='Pense, écrit, tape, romance, transcrit, compose, exprime, rédige, consigne, note... 🗒️'
+          onChange={e => handleChange(e.target.value)}
+          minRows={5}
+          multiline
+        ></TextField>
+      )}
     </div>
   );
 };
--- apps/front/src/shared/hooks/axios.config.ts
+++ apps/front/src/shared/hooks/axios.config.ts
@@ -16,18 +16,7 @@ export const configAxios = (props: {
   manual?: boolean;
   useCache?: boolean;
 }) => {
-  // Default value from API axios-hooks
   const { url, method, manual = false, autoCancel = true, useCache = false } = props;
   // eslint-disable-next-line react-hooks/rules-of-hooks
   return useAxios({ headers, url: axiosUrl(url), method }, { manual, autoCancel, useCache });
-};
-
-export const fetchApi = (url: string, method: 'POST' | 'GET' | 'PUT' | 'DELETE') => {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  fetch(axiosUrl(url), { headers: new Headers({ maple: 'true', 'Content-Type': 'application/json' }), method })
-    .then(e => {
-      console.log(e);
-    })
-    .catch(error => console.log(error))
-    .finally(() => true);
 };
