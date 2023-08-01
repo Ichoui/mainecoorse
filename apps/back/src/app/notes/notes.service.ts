c3a15f8c514264ff80af7648b56a0ab65d74eaba
Morgan Nicol <morganichoui@gmail.com>
2023-08-01 16:36:26 +0200

    fuckin notes!

--- apps/back/src/app/notes/notes.service.ts
+++ apps/back/src/app/notes/notes.service.ts
@@ -9,7 +9,15 @@ export class NotesService {
   constructor(@InjectRepository(NotesEntity) private _notesEntityRepository: Repository<NotesEntity>) {}
 
   async getNotes(): Promise<string> {
-    return this._notesEntityRepository.findOne({ where: { oneId: 1 } }).then(e => manageNoteData(e));
+    const entity: NotesEntity = await this._notesEntityRepository.findOne({ where: { oneId: 1 } });
+    if (!entity) {
+      const createEntity = this._notesEntityRepository.create({ oneId: 1, notes: '' });
+      return this._notesEntityRepository
+        .save(createEntity)
+        .then(() => '');
+    } else {
+      return manageNoteData(entity)
+    }
   }
 
   async postNotes(notesDto: NotesDto): Promise<string> {
