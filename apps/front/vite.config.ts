4ede302a6923ff80157fddfb3f536205ca6f2519
Morgan Nicol <morganichoui@gmail.com>
2023-08-10 20:50:12 +0200

    PWA added

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/front',
-  plugins: [tsconfigPaths(), react()],
-  envDir:'environments',
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    VitePWA({
+      // https://vite-pwa-org.netlify.app/examples/react.html
+      // https://github.com/vite-pwa/vite-plugin-pwa/
+      // https://web.dev/add-manifest/ manifest
+      // https://maskable.app/editor <-- pour générer les icones avec fond de couleur
+      // Utiliser lightHouse pour tester le PWA
+      registerType: 'autoUpdate',
+      workbox: {
+        // clientsClaim: true, // <-- this one generate sw.js and workbox-xxx.js
+        // skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  envDir: 'environments',
+  publicDir: 'public',
   server: {
     port: 1418,
     host: 'localhost',
