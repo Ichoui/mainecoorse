cee2bb1269732a7512ec2966f5555ccf73e639b2
Morgan Nicol <morganichoui@gmail.com>
2024-02-08 17:48:36 +0100

    oupsy ça compile mieux

--- apps/front/dev-dist/sw.js
+++ apps/front/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f16557dd'], (function (workbox) { 'use strict';
     "revision": "ea38d732354beb490034efbedf7f6d9f"
   }, {
     "url": "index.html",
-    "revision": "0.18au8f7oej"
+    "revision": "0.31s4jebijo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
