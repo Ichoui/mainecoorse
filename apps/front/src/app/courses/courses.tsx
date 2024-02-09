b2f52f714f62fed0ec5a666c86d2cb8043c17c6a
Morgan Nicol <morganichoui@gmail.com>
2024-02-09 14:11:24 +0100

    test gateway

--- apps/front/src/app/courses/courses.tsx
+++ apps/front/src/app/courses/courses.tsx
@@ -28,7 +28,7 @@ export const Courses = () => {
   const [{}, executePurge] = configAxios({ url: 'courses', method: 'DELETE', manual: true });
 
   useEffect(() => {
-    socket = io(import.meta.env.VITE_SOCKETIO, {  secure: true });
+    socket = io(import.meta.env.VITE_SOCKETIO, { transports: ['websocket'], secure: true });
     socket.on('fetchData', (res: { refetch: boolean; socketId: string }) => {
       if (res.refetch && socket.id !== res.socketId) {
         fetchCourses().then(e => setItemsSorted(sortByTags(e.data)));
